4105dac0bd843f43c1002797a652ceb80bcb8a35
faizghifari <fgh1997@live.com>
2019-06-17 22:55:36 +0700

    add quran data, fix pages, add error page, add quran-concept dict

--- app/app.py
+++ app/app.py
@@ -3,8 +3,11 @@ import pickle
 import numpy as np
 
 from flask import Flask, render_template, request
+from app.lib.dict import load_dict
 
 app = Flask(__name__)
+
+temp_dict = load_dict()
 
 @app.route('/')
 @app.route('/index')
@@ -15,10 +18,20 @@ def index():
 def main():
     return render_template('main.html')
 
-# @app.route('results')
-# def results():
-#     model = pickle.load('wordsim.pkl')
+@app.route('/results', methods=['POST'])
+def results():
+    if request.method == 'POST':
+        form = request.form
+        input_text = request.form['input-text']
 
+        print(input_text)
+        return render_template('results.html', input_text=input_text, form=form, temp_dict=temp_dict)
+    else:
+        return render_template('error.html')
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
